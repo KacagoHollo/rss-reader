0a478d1c05b2025d02b5e73fbe0c6bf2afd39b55
IstvanPongracz <pongracz88@gmail.com>
2021-10-22 17:33:23 +0200

    Setting flag

--- countries.js
+++ countries.js
@@ -6,6 +6,10 @@ function showCountry(country) {
     console.log(country);
     const name = country[0].name.official;
     document.getElementById("name").textContent=name;
+
+    const flag = country[0].flags.png;
+    document.getElementById("flag").setAttribute('src', flag);
+
 }
 
 window.addEventListener("load", loadCountry);
