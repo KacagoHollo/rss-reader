b2bed5236ec8b398bf03737c9d6f4c28249cf643
IstvanPongracz <pongracz88@gmail.com>
2021-10-22 19:32:20 +0200

    Plusz infók hozzáadása.

--- countries.js
+++ countries.js
@@ -7,8 +7,24 @@ function showCountry(country) {
     const name = country[0].name.official;
     document.getElementById("name").textContent=name;
 
+    for (let j = 0; j < country[0].name.nativeName.length; j++) {
+        const native = country[0].name.nativeName[j];
+        document.getElementById("nativeS").innerText += ' ' + native;
+   };
+    
+   const capital = country[0].capital;
+    document.getElementById("capital").innerHTML=capital;
+
     const flag = country[0].flags.png;
     document.getElementById("flag").setAttribute('src', flag);
+
+    const continent = country[0].continents[0];
+    document.getElementById("continent").innerHTML=continent;
+
+    for (let i = 0; i < country[0].timezones.length; i++) {
+       const tz = country[0].timezones[i];
+        document.getElementById("timezones").textContent += ' ' + tz;
+    }
 
 }
 
